3bf26ec85383aeee33a7de56f90dcd3587c167e9
Daniil Lysenko <productivemouse@gmail.com>
2022-01-28 20:13:39 +0300

    update files & add an excel ver

--- README.md
+++ README.md
@@ -1,21 +1,43 @@
 # defme - Find definitions easily
 ## What is it?
-Defme is a script written in Python, which allows a user to get definitions for words in Google Spreadsheets. It's pretty easy to install and very easy to use.
+Defme is a script written in Python, which allows a user to get definitions for words in Google Spreadsheets/Microsoft Excel. It's pretty easy to install and very easy to use.
 
 ## Installation:
+### Defme - Google Spreadsheets version
 1) Install these libraries:
-```
-pip install rich gspread PyDictionary
-```
+    ```
+    pip install rich gspread PyDictionary
+    ```
 2) Add a new project on Google Cloud, connecting to **Google Drive API** and **Google Spreadsheets API**. For a more descriptive tutorial on that, please watch this video: `link to be added...`
 3) Open `settings.py` and paste:
     * your `.json` file location in `sa_name` variable
     * name of your spreadsheet in `spreadsheet` variable
     * name of worksheet in `worksheet` variable
+4) Execute the script:
+    ```
+    python defme.py
+    ```
+5) You are ready to go!
+### Defme - Microsoft Excel version
+1) Install these libraries:
+```
+pip install rich gspread PyDictionary
+```
+2) Download and paste a file location into `settings.py` file    (*ignore* `sa_name` variable). Specify a worksheet you want to work with.    
+    Should look like this:
+    ```
+    spreadsheet = '/path-to-file/file'
+    worksheet = 'name-of-worksheet'
+    ```
+    You **should't** need to add file extension at the end!
+3) Run the script:
+    ```
+    python defme_excel.py
+    ```
 4) You are ready to go!
 
 ## Preview
 ![Error](resources/defme.gif)
 
 ## Notes
-Sometimes PyDictionary API returns a `List Out of Range error`. Simple solution: open `core.py` in PyDictionary lib's folder and change `disable_errors = True`. 
+While running the script you may see `Error: A Term must be only a single word` messages. Don't worry about them! *It's just the way PyDictionary module works.*
--- defme_excel.py
+++ defme_excel.py
@@ -1,10 +1,10 @@
 from rich.console import Console
 from rich.progress import track
 from rich.theme import Theme
-import gspread
+from openpyxl import Workbook, load_workbook
 import os
 import time
-from settings import spreadsheet, worksheet, sa_name
+from settings import spreadsheet, worksheet
 from PyDictionary import PyDictionary
 
 dictionary = PyDictionary()
@@ -18,9 +18,9 @@ os.system('cls' if os.name == 'nt' else 'clear')
 #TODO add synonyms & antonyms
 #TODO add examples <- create my own method 
 
-service_account = gspread.service_account(filename=sa_name)
-spread_sheet = service_account.open(spreadsheet)
-work_sheet = spread_sheet.worksheet(worksheet)
+
+workbook = load_workbook(f'{spreadsheet}.xlsx')
+worksheet = workbook[worksheet]
 
 possibles = ['Noun', 'Verb', 'Adjective', 'Adverb']
 not_single = []
@@ -32,14 +32,14 @@ ran = False
 def county():
     global rows_count, ran
     cnt_track = 0
-    for i in range(rows_count, work_sheet.row_count+1):
+    for i in range(rows_count, worksheet.max_row+1):
         if cnt_track >=10: # checking for the end of spreadsheet
             rows_count -= 10
             break
-        if (work_sheet.cell(i, 1).value == None):
+        if (worksheet[f'A{i}'].value == None):
             cnt_track += 1
             rows_count += 1
-        elif (work_sheet.cell(i, 1).value != None):
+        elif (worksheet[f'A{i}'].value != None):
             cnt_track = 0
             rows_count += 1
         time.sleep(0.1)
@@ -50,7 +50,7 @@ def county():
 
 def dict(i):
     result = ''
-    definition = dictionary.meaning(f'{work_sheet.cell(i, 1).value}', disable_errors=True)
+    definition = dictionary.meaning(f'{worksheet[f"A{i}"].value}', disable_errors=True)
     # print(definition)
     for i in possibles:
         try: 
@@ -67,34 +67,32 @@ def dict(i):
                     result += f'{pos+1}) ' + definition[i][pos] + '\n'
         except:
             pass
-
     return result.strip()
 
 def search(beg=2,end=rows_count): # current position
     global cur_pos, review
     for i in track(range(beg, rows_count), description='Processing...'):
         try: 
-            if len((work_sheet.cell(i, 1).value).split()) > 1:
+            if len((worksheet[f'A{i}'].value).split()) > 1:
                 not_single.append(f'A{i}')
         except:
             pass
-        if (work_sheet.cell(i, 1).value != None) and (work_sheet.cell(i, 2).value == None):
-            work_sheet.update(f'B{i}', dict(i))
-        elif (work_sheet.cell(i, 1).value == None) and (work_sheet.cell(i, 2).value != None): 
-            work_sheet.update(f'A{i}', 'REVIEW!!!')
+        if (worksheet[f'A{i}'].value != None) and (worksheet[f'B{i}'].value == None):
+            worksheet[f'B{i}'] = dict(i)
+        elif (worksheet[f'A{i}'].value == None) and (worksheet[f'B{i}'].value != None): 
+            worksheet[f'B{i}'].value = 'REVIEW!!!'
             review.append(f'A{i}:B{i}')
-            # print it using rich 
         cur_pos += 1
         time.sleep(0.1)
+
 
 def main():
     global ran
     if not ran:
         with console.status('Counting all words in cells...', spinner='material'):
             county()
-        with console.status('Cooldown', spinner='clock'):
-            time.sleep(5)
     search(beg=cur_pos)
+    workbook.save(f'{spreadsheet}.xlsx')
     console.print('Program sucessfully executed!', style='success')
     if review:
         console.print('\nDuring the execution these cells missing word but having definition were found:\n', style='warning')
@@ -109,13 +107,6 @@ if __name__ == '__main__':
         try:
             main()
             break
-        except gspread.exceptions.APIError as err:
-            console.print('Error on the API side occured!', style='error_api')
-            with console.status("Google API's has some restrictions! We're sorry!", spinner='monkey'):
-                time.sleep(20)
-            # print(err)
-        except gspread.exceptions.CellNotFound as err:
-            console.print('Cell was not found!', style='error')
-            pass
-        except gspread.exceptions.WorksheetNotFound as err:
-            console.print('Worksheet does not exist or not acessible!', style='error')
+        except:
+            console.print("An error has occured!", style='error')
+            break
